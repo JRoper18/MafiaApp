84b160973e1f511c2aa72cdb5971ecfeacb2176c
UnusualArnen <jack@skroper.com>
2016-07-30 20:27:23 -0500

    Started transition from a host/lobby based connection to auto-joining.

--- Mafia/LobbyViewController.swift
+++ Mafia/LobbyViewController.swift
@@ -9,9 +9,7 @@
 import UIKit
 import MultipeerConnectivity;
 
-class SearchForGameViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-
+class LobbyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let advertiser = GameConnectionAdvertiser();
     @IBOutlet weak var tableView: UITableView!
@@ -23,12 +21,13 @@ class SearchForGameViewController: UIViewController, UITableViewDelegate, UITabl
         tableView.reloadData();
         
     }
+    func addConnectionButton(){
+        
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return advertiser.nearbyHosts.count;
     }
-    
-    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("HostCell", forIndexPath: indexPath)
