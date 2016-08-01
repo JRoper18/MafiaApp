bc59902abcef2f86833b20b9ed1a9e93fc4491c0
UnusualArnen <jack@skroper.com>
2016-07-31 22:08:25 -0500

    Figured out that sendData wasn't working because I forgot to delegate. Again. *Internally crying*

--- Mafia/WaitingForPlayersViewController.swift
+++ Mafia/WaitingForPlayersViewController.swift
@@ -14,33 +14,31 @@ class WaitingForPlayersViewController: UIViewController, MCSessionDelegate {
     override func viewDidLoad(){
         super.viewDidLoad();
         let playerJoinNameString = "PlayerJoin:" + deviceSession.myPeerID.displayName;
-        do{
-            try deviceSession.sendData(playerJoinNameString.dataUsingEncoding(NSUTF8StringEncoding)!, toPeers: deviceSession.connectedPeers, withMode: .Reliable);
-
-        } catch {
-            print("An error occured while sending data");
-        }
+        deviceSession.delegate = self;
+        try! deviceSession.sendData(String("GameStart").dataUsingEncoding(NSUTF8StringEncoding)!, toPeers: deviceSession.connectedPeers, withMode: .Unreliable);
+        print(deviceSession.connectedPeers.count)
+        
     }
     func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
-        let command = String(data);
-        
-        if command.substringToIndex(command.startIndex.advancedBy(10)) == "PlayerJoin:"{
-            players.append(String(data: data, encoding: NSUTF8StringEncoding)!);
-            //If all the players are in the ready screen
-            if(players.count == session.connectedPeers.count){
-                let commandString = "GameStart"
-                do{
-                    try session.sendData(commandString.dataUsingEncoding(NSUTF8StringEncoding)!, toPeers: deviceSession.connectedPeers, withMode: .Reliable);
-                } catch {
-                    print("An error occured while sending data");
+        //Aparently the dispatch async thing makes this work.
+        print("Got data");
+        dispatch_async(dispatch_get_main_queue()) {
+            let command = String(data);
+            if command == "PlayerJoin:"{
+                self.players.append(peerID.displayName);
+                //If all the players are in the ready screen
+                if(self.players.count == session.connectedPeers.count){
+                    self.performSegueWithIdentifier("StartGame", sender: nil);
+
                 }
             }
-        }
-        else if command == "GameStart"{
-            
-        }
-        else{
-            print(command);
+            else if command == "GameStart"{
+                print("EEEY");
+                self.performSegueWithIdentifier("StartGame", sender: nil);
+            }
+            else{
+
+            }
         }
     }
     func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
