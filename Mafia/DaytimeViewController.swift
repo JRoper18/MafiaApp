824144ad9edb24a6b181c9b219e57eba31a86072
Saurav Desai <esowmya@sauravs-macbook-pro-2.local>
2016-08-01 13:15:45 -0500

    added daytime vc

---
+++ Mafia/DaytimeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DaytimeViewController.swift
+//  Mafia
+//
+//  Created by Saurav Desai on 8/1/16.
+//  Copyright © 2016 Jack Roper. All rights reserved.
+//
+
+import UIKit
+
+class DaytimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
