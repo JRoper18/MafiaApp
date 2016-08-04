4887a6fcfb673ab09612d938d45c44e487f0b445
UnusualArnen <jack@skroper.com>
2016-08-03 20:54:32 -0500

    I FINALLY DID IT i don't even know why my solution works but death messages now are sent correctly during nighttime.

--- Mafia/NighttimeViewController.swift
+++ Mafia/NighttimeViewController.swift
@@ -16,7 +16,7 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
     @IBOutlet weak var playerRevealLabel: UILabel!
     
     var targetPlayers : [Player] = []
-    var selectedPlayer : String = ""
+    var selectedPlayer : String = "";
     var time : Int = 0
     var hunterHasChecked : Bool = false
     
@@ -39,6 +39,9 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
         
         getTargetPlayers()
         
+        if thisPlayer.role != .Townsman{
+            selectedPlayer = targetPlayers[0].name
+        }
         pickerView.delegate = self
         pickerView.dataSource = self
         
@@ -69,17 +72,29 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
     func sendDecision(){
         self.sentDecision = true;
         self.timer.invalidate();
-        let dataToSend = "HERE IS ACTION".dataUsingEncoding(NSUTF8StringEncoding)
+        let dataToSend = String("HEREISACTION").dataUsingEncoding(NSUTF8StringEncoding)
         for player in players{
             if thisPlayer.roleToString() == "Pirate" {
-                
+                print("Looking for player " + player.name + " with id " + selectedPlayer);
                 if player.name == selectedPlayer{
                     //Just send them the stuff.
-                    let playerID = MCPeerID(displayName: player.name)
+                    let playerID = MCPeerID(displayName: selectedPlayer)
                     do{
-                        try deviceSession.sendData(dataToSend!, toPeers: [playerID], withMode: .Unreliable)
+                        for peer in deviceSession.connectedPeers{
+                            if peer.displayName == selectedPlayer{
+                                
+                                //So this works, but using playerID to send doesnt. I dont even know why.
+                                print(deviceSession.connectedPeers[0].displayName);
+                                try deviceSession.sendData(dataToSend!, toPeers: [peer], withMode: .Unreliable)
+                                //This is an error, but SOMEHOW the prints below say that the only device IS THE ONE IM SENDING TO
+                                
+                            }
+                        }
+                        
                     } catch {
-                        print("Pirate data transfer failed");
+                        print(deviceSession.connectedPeers[0].displayName);
+                        //The statement above prints "pad". The one below prints "no device 'pad'". ARGH
+                        print(String(error));
                     }
                 }
             }
@@ -89,7 +104,11 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
                     let playerID = MCPeerID(displayName: player.name)
                     //Tell the guy that we heal him, tlel everyone that we chose our heal target.
                     do{
-                        try deviceSession.sendData(dataToSend!, toPeers: [playerID], withMode: .Unreliable)
+                        for peer in deviceSession.connectedPeers{
+                            if peer.displayName == selectedPlayer{
+                                try deviceSession.sendData(dataToSend!, toPeers: [peer], withMode: .Unreliable)
+                            }
+                        }
                     } catch {
                         print("Healer data transfer failed");
                     }
@@ -123,6 +142,8 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
     
     func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
         dispatch_async(dispatch_get_main_queue()) {
+            let alert = UIAlertController(title: "GOT ACTION", message: "EEEY", preferredStyle: .Alert)
+            self.presentViewController(alert, animated: true, completion: nil);
             print("Got action from " + peerID.displayName)
             if !self.sentDecision{
                 self.sendDecision()
@@ -172,6 +193,7 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
         
     }
     func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        print("Connected user " + peerID.displayName + "has changed state. ")
         if state == MCSessionState.NotConnected {
             for index in 0..<players.count{
                 if players[index].name == peerID.displayName {
@@ -191,7 +213,11 @@ class NighttimeViewController: UIViewController, UIPickerViewDataSource, UIPicke
     
     func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
     }
-    
+    func session(session: MCSession, didReceiveCertificate certificate: [AnyObject]?, fromPeer peerID: MCPeerID, certificateHandler: (Bool) -> Void){
+        certificateHandler(true);
+        
+        
+    }
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         return targetPlayers.count
     }
